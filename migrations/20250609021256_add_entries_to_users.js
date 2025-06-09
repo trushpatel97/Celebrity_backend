577c4b16f83b9306a2511cf9ae42fcdf520df9d8
trushpatel97 <trushpatel1997@gmail.com>
2025-06-08 22:13:54 -0400

    Feat: Add entries column to users table and run migration

---
+++ migrations/20250609021256_add_entries_to_users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('users', function(table) {
+    table.integer('entries').notNullable().defaultTo(0);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('users', function(table) {
+    table.dropColumn('entries');
+  });
+};
