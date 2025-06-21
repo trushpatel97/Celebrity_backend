d5bdaf53ba5eb4ecf7421e244f5b29987056dfb0
Trush <trushpatel1997@gmail.com>
2025-06-20 22:21:45 -0400

    Fix axios installation

--- server.js
+++ server.js
@@ -1,54 +1,29 @@
-require('dotenv').config();
-const pgp = require('pg-promise')();
-const winston = require('winston');
 const express = require('express'); //using express for backend
 const bodyParser = require('body-parser');//body parser is used to convert to json
 const bcrypt = require('bcrypt-nodejs');//used for hashing the passwords to make them more secure
 const cors = require('cors');//allows website communication between two different websites
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-const axios = require('axios');
-// Initialize the database connection
-const db = pgp(process.env.DATABASE_URL);
-
-// Test the connection
-db.connect()
-  .then((obj) => {
-    console.log('Connected to the database successfully');
-    obj.done(); // Release the connection
-  })
-  .catch((error) => {
-    console.error('Database connection error:', error);
-  });
-
-// Example query
-db.any('SELECT NOW()')
-  .then((result) => {
-    console.log('Query result:', result);
-  })
-  .catch((error) => {
-    console.error('Query error:', error);
-  });
-
+const knex = require('knex')//allows us to connect our database to the backend
 
 const register = require('./controller/register');
 const signin = require('./controller/signin');
 const profile = require('./controller/profile');
 const image = require('./controller/image');
 
-// Use process.env.CLARIFAI_PAT wherever needed for Clarifai API integration.
+const db = knex({//using knex
+  client: 'pg',//we will be using postgreSQL 
+  connection: {
+    connectionString: "postgresql://celebritydb_user:vISaM0dKJZ7Fz42pSyihw9iXF5tnxom3@dpg-d1b0irodl3ps73e5l2n0-a/celebritydb",
+    ssl: {
+    rejectUnauthorized: false,
+    }
+  },
+});
 
 const app = express();//initializing the app to use express
 
 app.use(cors())//using cors to allow site communication
 app.use(bodyParser.json());//parsing to json
-const allowedOrigins = ['http://localhost:3000', 'https://celebrityalike-aa43d85f8ff5.herokuapp.com'];
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-// Handle preflight requests
-app.options('*', cors());
+
 app.get('/', (req, res)=> {//home or root of the site
   res.send(db.users);//send over the database users
 })
@@ -66,17 +41,3 @@ app.listen(process.env.PORT || 3000, ()=> {//listen to port 3000
   console.log('app is running on port 3000');
 })
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-  ],
-});
-
-logger.info('Application started');
-logger.error('An error occurred');
